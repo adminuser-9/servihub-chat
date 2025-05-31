9283a6f74ee4d90d6c86e07ef71237522ccdc98d
Boolwark <72696784+Boolwark@users.noreply.github.com>
2025-05-31 16:31:42 +0800

    add global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,14 @@
+// global.d.ts
+import 'fastify';
+import Redis from 'ioredis';
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    redis: Redis;
+    jwt: {
+      sign: (...args: any[]) => string;
+      verify: (...args: any[]) => any;
+      decode: (...args: any[]) => any;
+    };
+  }
+}
